c75cd4e4f782e65377c5f1679faaf61a852b4828
ivanidze1235 <ivanidzedjan2@gmail.com>
2025-01-28 21:29:07 +0000

    added bootstrap, set up basic styles

--- pizzaplace/pizzaplace/settings.py
+++ pizzaplace/pizzaplace/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pizza_app', # my app
-    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static/'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
